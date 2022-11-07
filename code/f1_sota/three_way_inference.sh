f0ebc097cf7d6adc852b6d5af3043cbf16763cea
gyuboone <48861154+gyuboone@users.noreply.github.com>
2022-11-07 16:40:07 +0900

    [Delete] Remove old file

--- code/f1_sota/three_way_inference.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-SM_CHANNEL_EVAL="/opt/ml/input/data/eval" SM_CHANNEL_MODEL="model/exp49" SM_OUTPUT_DATA_DIR="inference_output"  \
-python inference.py --model EfficientB4 \
---single True \
---tta True
-# --age_dir "saved_models/joint_exp/age49" \
-# --gender_dir "saved_models/joint_exp/gender22" \
-# --mask_dir "saved_models/joint_exp/mask23"
