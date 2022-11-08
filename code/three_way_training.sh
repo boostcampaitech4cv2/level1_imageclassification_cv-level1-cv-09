0f1f17d746844121a980395cc0675d98a42150f7
byj9402 <byj9402@naver.com>
2022-11-08 07:28:10 +0000

    Revert "remove old files"
    This reverts commit dccda86f3fbef5dd8013acc70c556f7dc2461bc1.

--- code/three_way_training.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# SM_CHANNEL_TRAIN="/opt/ml/input/data/train/images" SM_MODEL_DIR="saved_models/joint_exp" python train.py  --dataset AgeProfileOnlyDataset  --name "age" --cutmix "yes"
-# SM_CHANNEL_TRAIN="/opt/ml/input/data/train/images" SM_MODEL_DIR="saved_models/joint_exp" python train.py  --dataset GenderProfileOnlyDataset  --name "gender" --cutmix "yes"
-# SM_CHANNEL_TRAIN="/opt/ml/input/data/train/images" SM_MODEL_DIR="saved_models/joint_exp" python train.py  --dataset MaskProfileOnlyDataset  --name "mask" --cutmix "yes"
-
-#SINGLE
-#SM_CHANNEL_TRAIN="/opt/ml/input/data/train/images" SM_MODEL_DIR="saved_models" python train.py  --dataset MaskProfileOnlyDataset  --name "exp" --cutmix "yes"
-SM_CHANNEL_TRAIN="/opt/ml/input/data/train/images" SM_MODEL_DIR="saved_models" python train.py  --dataset MaskSplitByProfileDataset  --name "exp" --model "EfficientB4" --cutmix "yes"
-#SM_CHANNEL_TRAIN="/opt/ml/input/data/train/images" SM_MODEL_DIR="saved_models" python train.py  --dataset MaskBaseDataset  --name "exp" 
